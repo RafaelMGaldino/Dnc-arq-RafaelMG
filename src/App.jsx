f242fd9557505b41cf6a3e74dbe10e3f3ad9b38c
RafaelMGaldino <101841163+RafaelMGaldino@users.noreply.github.com>
2025-06-16 17:38:38 -0300

    rotar homer mudada

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       <ScrollTop />
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
